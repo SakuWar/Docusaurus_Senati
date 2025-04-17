936701fb1c33344824549c7304e065b09594749f
Diego Manuel Chuquiruna Abanto <146387062+SakuWar@users.noreply.github.com>
2025-04-17 12:39:34 -0500

    Segundo commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,15 +10,15 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Docusaurus Senati',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://967876949.facturalo.co',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
